ba2fcabaf9f176e83dde77dbe282a5d6287dba91
pisalore <lorenzo.pisaneschi@gmail.com>
2020-01-04 11:50:48 +0100

    Add project files.

---
+++ MeanShift_CUDA/commonDefines.h
@@ -0,0 +1,14 @@
+#ifndef COMMONDEFINES_H
+#define COMMONDEFINES_H
+
+#define MAX_ITERATIONS 15
+#define BW 1.5
+#define TH 10.*BW
+
+#define TILE_WIDTH 8
+#define BLOCK_DIM TILE_WIDTH
+
+#endif // !COMMONDEFINES_H
+
+
+//block dim 32 for no shared memory version
